monero-project/monero

wallet: create set_subaddress_lookahead wallet rpc endpoint

src/wallet/wallet_rpc_server_commands_defs.h
@@ -182,6 +182,27 @@ namespace wallet_rpc
     typedef epee::misc_utils::struct_init<response_t> response;
   };
 
+  struct COMMAND_RPC_SET_SUBADDR_LOOKAHEAD
+  {
+    struct request_t
+    {
+      uint64_t major_idx;
+      uint64_t minor_idx;
+      BEGIN_KV_SERIALIZE_MAP()
+        KV_SERIALIZE(major_idx)
+        KV_SERIALIZE(minor_idx)
+      END_KV_SERIALIZE_MAP()
+    };
+    typedef epee::misc_utils::struct_init<request_t> request;
+
+    struct response_t
+    {
+      BEGIN_KV_SERIALIZE_MAP()
+      END_KV_SERIALIZE_MAP()
+    };
+    typedef epee::misc_utils::struct_init<response_t> response;
+  };
+
   struct COMMAND_RPC_CREATE_ADDRESS
   {
     struct request_t

Shekyl-Foundation/shekyl-core

src/wallet/wallet_rpc_server_commands_defs.h
@@ -47,7 +47,7 @@
 // advance which version they will stop working with
 // Don't go over 32767 for any of these
 #define WALLET_RPC_VERSION_MAJOR 1
-#define WALLET_RPC_VERSION_MINOR 28
+#define WALLET_RPC_VERSION_MINOR 29
 #define MAKE_WALLET_RPC_VERSION(major,minor) (((major)<<16)|(minor))
 #define WALLET_RPC_VERSION MAKE_WALLET_RPC_VERSION(WALLET_RPC_VERSION_MAJOR, WALLET_RPC_VERSION_MINOR)
 namespace tools
@@ -177,6 +177,27 @@ namespace wallet_rpc
       cryptonote::subaddress_index index;
       BEGIN_KV_SERIALIZE_MAP()
         KV_SERIALIZE(index)
+      END_KV_SERIALIZE_MAP()
+    };
+    typedef epee::misc_utils::struct_init<response_t> response;
+  };
+
+  struct COMMAND_RPC_SET_SUBADDR_LOOKAHEAD
+  {
+    struct request_t
+    {
+      uint32_t major_idx;
+      uint32_t minor_idx;
+      BEGIN_KV_SERIALIZE_MAP()
+        KV_SERIALIZE(major_idx)
+        KV_SERIALIZE(minor_idx)
+      END_KV_SERIALIZE_MAP()
+    };
+    typedef epee::misc_utils::struct_init<request_t> request;
+
+    struct response_t
+    {
+      BEGIN_KV_SERIALIZE_MAP()
       END_KV_SERIALIZE_MAP()
     };
     typedef epee::misc_utils::struct_init<response_t> response;