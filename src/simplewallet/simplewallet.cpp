9683e39ca90c2d9fddaca29c543723c666832b0c
WHR <whr@rivoreo.one>
2025-12-02 10:32:50 +0800

    simplewallet: report file writing failure for export_transfers command

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -9321,6 +9321,10 @@ bool simple_wallet::export_transfers(const std::vector<std::string>& args_)
   }
 
   std::ofstream file(filename);
+  if(file.fail()) {
+    fail_msg_writer() << boost::format(tr("Failed to open %s for writing")) % filename;
+    return true;
+  }
 
   // header
   file <<
@@ -9390,7 +9394,11 @@ bool simple_wallet::export_transfers(const std::vector<std::string>& args_)
   }
   file.close();
 
-  success_msg_writer() << tr("CSV exported to ") << filename;
+  if(file.fail()) {
+    fail_msg_writer() << tr("Failed to export CSV to ") << filename;
+  } else {
+    success_msg_writer() << tr("CSV exported to ") << filename;
+  }
 
   return true;
 }
